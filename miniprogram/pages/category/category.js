85b3c5400284117253fd9f8d12ecb6375be96871
jdzhrch <825029878@qq.com>
2019-06-03 10:20:59 +0800

    最终版

--- miniprogram/pages/category/category.js
+++ miniprogram/pages/category/category.js
@@ -5,15 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    articles: [{
-      title: "李光耀去世",
-      url: "www.baidu.com",
-      pv: 3000
-    }, {
-      title: "李光耀去世啊啊啊啊",
-      url: "www.baidu.com",
-      pv: 1234
-    }],
+    articles: [],
     isHideLoadMore: false
   },
 
